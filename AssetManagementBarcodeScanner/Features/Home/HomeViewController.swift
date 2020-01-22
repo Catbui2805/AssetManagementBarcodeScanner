1bae14626734bb3226390f7c7c832e12cac37301
Nguyen Tran Cong <Catbui2805@gmail.com>
2020-01-23 01:06:45 +0700

    Read QR code

--- AssetManagementBarcodeScanner/Features/Home/HomeViewController.swift
+++ AssetManagementBarcodeScanner/Features/Home/HomeViewController.swift
@@ -19,18 +19,32 @@ class HomeViewController: UIViewController {
         return lb
     }()
     
+    var btQrScanner: UIButton = {
+        let bt = UIButton()
+        bt.translatesAutoresizingMaskIntoConstraints = false
+        bt.backgroundColor = .blue
+        bt.setTitle("QR Scanner", for: .normal)
+        bt.titleLabel?.textColor = .white
+        return bt
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupViews()
-
+        
     }
-
+    
+    @objc func tappedQRScanner() {
+        let vc = QRScannerViewController()
+        vc.title = "Scanner"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
 private extension HomeViewController {
     func setupViews() {
         setupLabel()
+        setupButtonQRScanner()
     }
     
     func setupLabel() {
@@ -42,4 +56,16 @@ private extension HomeViewController {
             lb.leadingAnchor.constraint(equalTo: view.leadingAnchor)
         ])
     }
+    
+    func setupButtonQRScanner() {
+        view.addSubview(btQrScanner)
+        btQrScanner.addTarget(self, action: #selector(tappedQRScanner), for: .touchUpInside)
+        NSLayoutConstraint.activate([
+            btQrScanner.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            btQrScanner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            btQrScanner.heightAnchor.constraint(equalToConstant: 44),
+            btQrScanner.widthAnchor.constraint(equalToConstant: view.bounds.width - (60 * 2))
+        ])
+    }
 }
+
--- AssetManagementBarcodeScanner/Supporting Files/SceneDelegate.swift
+++ AssetManagementBarcodeScanner/Supporting Files/SceneDelegate.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -22,7 +22,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         let home = HomeViewController()
         home.view.backgroundColor = .white
-        window?.rootViewController = home
+        
+        let navigationController = UINavigationController(rootViewController: home)
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
     }
